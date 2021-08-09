9c7f3946ea1219aa14031b8da19a03d193a67386
13jon37 <jonlockllyr2@gmail.com>
2021-08-09 09:30:16 -0400

    code

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pushd build
-gcc -c ../src/*.c -g -Wall && gcc *.o -o a.out -lSDL2main -lSDL2 -lSDL2_image 
+g++ -c ../src/*.cpp -g -Wall && g++ *.o -o a.out -lSDL2main -lSDL2 -lSDL2_image 
 popd
